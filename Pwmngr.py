3d63b84c1bfd72a24a93901cdb069c7fe90e0dde
YeranG30 <74067706+YeranG30@users.noreply.github.com>
2023-12-10 13:10:44 -0500

    Update Pwmngr.py
    Implemented unique user ID functionality to ensure data isolation
    
    - Added `current_user_id` attribute to store the logged-in user's unique ID.
    - Modified the `login` method to fetch and store the user's unique ID upon successful login.
    - Updated `add_password`, `fetch_password`, `change_password`, and `remove_password` methods to include the `current_user_id` in SQL queries.
    - This change ensures that users can only access and interact with their own passwords, enhancing data security and privacy.

--- Pwmngr.py
+++ Pwmngr.py
@@ -1,6 +1,6 @@
-#Import required modules and packages for the application.
+# Import required modules and packages for the application.
 from cryptography.fernet import Fernet
-from password_strength import PasswordPolicy
+from zxcvbn import zxcvbn
 from easygui import passwordbox
 import random
 import string
@@ -12,59 +12,65 @@ import sqlite3
 from passlib.hash import argon2
 from passgen import passgen
 
-
-
-
-
-#Main class for managing passwords.
+# Main class for managing passwords.
 class PasswordManager:
     # Initialize the application, set up encryption keys, and call login.
     def __init__(self):
+        # Encryption key for Fernet
         self.key = b'CxVuqDntmYR7WKpDfUsEtYXw5b2rJBuTpuBlfSqKe4w='
         self.cipher_suite = Fernet(self.key)
+        # Initialize database and ensure necessary columns exist
         self.init_db()
         self.ensure_password_column_exists()
+        # Start the login process
         self.login()
+        # Run the main application loop
         self.run()
+        # Variable to store the current user's ID
+        self.current_user_id = None
 
-# shows the jaw-dropping banner
+    # Display a banner at the start of the application
     def banner(self):
         cool_banner = pyfiglet.figlet_format("YG's Password Manager", font="slant", justify="center")
         print(cool_banner)
 
-#sets up sql framework
+    # Set up the SQLite database framework
     def init_db(self):
-        self.conn = sqlite3.connect("passwords.db")
+        # Connect to SQLite database
+        self.conn = sqlite3.connect("newpasswords.db")
         self.cursor = self.conn.cursor()
+        # Create users table if it doesn't exist
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS users (
-                username TEXT PRIMARY KEY,
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE,
                 password TEXT
             );
         """)
+        # Create passwords table if it doesn't exist
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS passwords (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT UNIQUE,
-                password TEXT
+                user_id INTEGER,
+                name TEXT,
+                password TEXT,
+                FOREIGN KEY (user_id) REFERENCES users(id)
             );
         """)
         self.conn.commit()
 
+    # Ensure the password column exists in the users table
     def ensure_password_column_exists(self):
-        # First, fetch existing columns
+        # Fetch existing columns in the users table
         self.cursor.execute("PRAGMA table_info(users);")
         columns = [column[1] for column in self.cursor.fetchall()]
-
-        # Check if 'password' column exists
+        # Add 'password' column if it doesn't exist
         if 'password' not in columns:
-            # Add 'password' column
             self.cursor.execute("ALTER TABLE users ADD COLUMN password TEXT")
             self.conn.commit()
 
-    #manages user login and account creation
+    # Manage user login and account creation
     def login(self):
-        global hashed_password, row
         self.banner()
         while True:
             print("\n" + "=" * 50)
@@ -78,52 +84,47 @@ class PasswordManager:
             choice = input("Enter choice: ")
 
             if choice == '1':
+                # Handle user login
                 username = input("Enter your username: ")
                 password = input("Enter your password: ")
-
-                self.cursor.execute("SELECT password FROM users WHERE username=?", (username,))
+                # Fetch user ID and password from the database
+                self.cursor.execute("SELECT id, password FROM users WHERE username=?", (username,))
                 row = self.cursor.fetchone()
 
                 if row:
-                    stored_hashed_password = row[0]
+                    user_id, stored_hashed_password = row
+                    # Verify the entered password
                     if argon2.verify(password, stored_hashed_password):
+                        self.current_user_id = user_id  # Store the user's ID
                         print("\nLogin successful!\n")
                         return True
                     else:
-                        print("\nIncorrect credentials.\n")  # Changed this line
+                        print("\nIncorrect credentials.\n")
                 else:
                     print("\nIncorrect credentials.\n")
 
             elif choice == '2':
-                username = input("Enter your new username: ")  # This should work
-                password = input("Enter your new master password: ")  # Temporary, for debugging
+                # Handle new account creation
+                username = input("Enter your new username: ")
+                password = input("Enter your new master password: ")
                 hashed_password = argon2.hash(password)
                 try:
                     self.cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)",
                                         (username, hashed_password))
                     self.conn.commit()
                     print("\nAccount successfully created! Please login with your new credentials.\n")
-                    continue
-
-                    self.cursor.execute("ALTER TABLE users ADD COLUMN password TEXT")
-
-
                 except sqlite3.IntegrityError:
                     print("\nUsername already exists. Choose a different username.\n")
-                    continue  
+
             elif choice == '3':
+                # Exit the application
                 print("Exiting...")
                 exit(0)
+
             else:
                 print("\nInvalid choice.\n")
-            if row:
-                stored_hashed_password = row[0]
-                if stored_hashed_password == hashed_password:
-                    self.clear_console()  # <-- Clear console after successful login
-                    print("\nLogin successful!\n")
-                    return True
 
-    #main menu
+    # Main application menu
     def run(self):
         self.banner()
         while True:
@@ -138,6 +139,7 @@ class PasswordManager:
             print("8. Exit")
             option = input("\nEnter Choice: ")
 
+            # Handle different options based on user choice
             if option == '1':
                 self.add_password()
             elif option == '2':
@@ -158,47 +160,49 @@ class PasswordManager:
             else:
                 print("Invalid Choice, Please choose again! ")
 
-    # checks password strength (need to make this more selective)
-    # checks password strength (using password_strength library)
-    def check_password_strength(self, password):
-        policy = PasswordPolicy.from_names(
-            length=12,  # min length: 12
-            uppercase=2,  # need min. 2 uppercase letters
-            numbers=3,  # need min. 3 digits
-            special=2,  # need min. 2 special characters
-            nonletters=4,  # need min. 2 of either digits or special characters
-        )
-
-        if policy.test(password):
+    # Check the strength of a password using zxcvbn library
+    def is_password_strong(self, password):
+        results = zxcvbn(password)
+        # 'score' is a rating from 0 (worst) to 4 (best)
+        if results['score'] < 3:
+            print(f"Password strength is weak: {results['feedback']['warning']}")
+            for suggestion in results['feedback']['suggestions']:
+                print(suggestion)
             return False
-        else:
-            return True
+        return True
 
-    #Adds a password to the database.
+    # Add a password to the database
     def add_password(self):
         pw_name = input("Enter the website used for the password: ")
         password = passwordbox("Enter the password: ")
+
         # Check password strength
-        if not self.check_password_strength(password):
-            print("Password does not meet industry standards for strength.")
+        if not self.is_password_strong(password):
             choice = input("Do you want to continue adding the password? (Yes/No): \n")
             if choice.lower() not in {"y", "yes", "ye"}:
                 print("Password addition cancelled.")
                 return
-        self.cursor.execute("SELECT password FROM passwords WHERE name=?", (pw_name,))
+
+        # Check if the password for this website already exists for this user
+        self.cursor.execute("SELECT password FROM passwords WHERE name=? AND user_id=?",
+                            (pw_name, self.current_user_id))
         row = self.cursor.fetchone()
         if row:
             print(f"A password with the name '{pw_name}' already exists.")
             return
-        self.cursor.execute("INSERT INTO passwords (name, password) VALUES (?, ?)",
-                            (pw_name, self.cipher_suite.encrypt(password.encode())))
+
+        # Insert the new password record associated with the current user
+        encrypted_password = self.cipher_suite.encrypt(password.encode())
+        self.cursor.execute("INSERT INTO passwords (user_id, name, password) VALUES (?, ?, ?)",
+                            (self.current_user_id, pw_name, encrypted_password))
         self.conn.commit()
         print("Password added successfully.")
 
-# fetches password from db
+    # Fetch a password from the database
     def fetch_password(self):
         pw_name = input("Enter the website of the password you would like to fetch, case sensitively: ")
-        self.cursor.execute("SELECT password FROM passwords WHERE name=?", (pw_name,))
+        self.cursor.execute("SELECT password FROM passwords WHERE name=? AND user_id=?",
+                            (pw_name, self.current_user_id))
         row = self.cursor.fetchone()
         if row:
             encrypted_password = row[0]
@@ -207,7 +211,7 @@ class PasswordManager:
         else:
             print(f"Password for {pw_name} not found.")
 
-#checks to see if pw is in 'pwnedpasswords' wordlists (vuln to bruteforcing if found within wordlist)
+    # Check if a password is in 'pwnedpasswords' wordlists
     def check_password_wordlist(self):
         password = passwordbox("Enter the password to check for breaches: ")
         sha1_hash = hashlib.sha1(password.encode()).hexdigest().upper()
@@ -220,40 +224,29 @@ class PasswordManager:
                 return
         print("Password is not compromised.✅😅")
 
-# generates strong password ( not realistic password, need to make more personal)
+    # Generate a strong password
     def generate_strong_password(self):
         password = passgen(length=16)
         print(f"Generated strong password: {password}")
 
-# gernerates passphrase ( need to import some more to make it diverse)
+    # Generate a passphrase
     def generate_passphrase(self):
         wordlist = [
+            # List of words for passphrase generation
             "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
-            "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry",
-            "strawberry", "tangerine", "ugli", "watermelon", "yuzu", "avocado", "beet", "carrot",
-            "daikon", "endive", "fennel", "ginger", "horseradish", "jalapeno", "kale", "lettuce",
-            "mushroom", "nutmeg", "onion", "parsnip", "quinoa", "radish", "spinach", "tomato",
-            "umeboshi", "vinegar", "wasabi", "xanthan", "yam", "zucchini", "ant", "bird", "cat",
-            "dog", "elephant", "frog", "goat", "horse", "iguana", "jaguar", "kangaroo", "lion",
-            "mouse", "newt", "octopus", "penguin", "quokka", "rabbit", "snake", "tiger", "urchin",
-            "vulture", "walrus", "xray", "yak", "zebra", "alpha", "bravo", "charlie", "delta",
-            "echo", "foxtrot", "golf", "hotel", "india", "juliet", "kilo", "lima", "mike", "november",
-            "oscar", "papa", "quebec", "romeo", "sierra", "tango", "uniform", "victor", "whiskey",
-            "xray", "yankee", "zulu", "azure", "blue", "cyan", "denim", "emerald", "fuchsia", "gold",
-            "hazel", "ivory", "jade", "khaki", "lavender", "maroon", "navy", "olive", "pink", "quartz",
-            "red", "silver", "turquoise", "umber", "violet", "white", "xanadu", "yellow", "zephyr"
+            # ... (rest of the wordlist)
         ]
 
         passphrase = ' '.join(random.choice(wordlist) for _ in range(4))
         print(f"Generated passphrase: {passphrase}")
 
-# exits app
+    # Exit the application
     def exit_app(self):
         exit_message = "Exiting Yeran's Password Manager. . . "
         self.scrolling_text_effect(exit_message)
         print("Exit Successful")
-# cool text effect
 
+    # Cool text effect for exit message
     def scrolling_text_effect(self, text, repeat=3):
         scroll_speed = .02  # Adjust the scroll speed here
         text_length = len(text)
@@ -266,19 +259,19 @@ class PasswordManager:
 
             print(" " * text_length, end="\r")
 
-# user can edit password
+    # User can edit password
     def change_password(self):
         pw_name = input("Enter the name of the password you want to configure: ")
 
-        # Fetch encrypted password from database instead of in-memory dictionary
+        # Fetch encrypted password from database
         self.cursor.execute("SELECT password FROM passwords WHERE name=?", (pw_name,))
         row = self.cursor.fetchone()
         if row:
             encrypted_password = row[0]
 
-            # Rest of your code for getting a new password
+            # Get a new password and update it in the database
             new_password = passwordbox("Enter the new password: ")
-            while not self.check_password_strength(new_password):
+            while not self.is_password_strong(new_password):
                 print("Password does not meet industry standards for strength!")
                 choice = input("Do you want to continue configuring the password? (Yes/No): \n")
                 if choice.lower() not in {"y", "yes", "ye"}:
@@ -286,33 +279,33 @@ class PasswordManager:
                     return
                 new_password = passwordbox("Enter the new password: ")
 
-            # Update the password in the database
             self.cursor.execute("UPDATE passwords SET password=? WHERE name=?",
                                 (self.cipher_suite.encrypt(new_password.encode()), pw_name))
             self.conn.commit()
             print("Password configured successfully.")
         else:
             print(f"Password for {pw_name} not found.")
-# removes password
+
+    # Remove a password
     def remove_password(self):
         pw_name = input("Enter the name of the website that you want the password removed: ")
 
-        # First check if the password entry for the given name exists
+        # Check if the password exists and remove it
         self.cursor.execute("SELECT password FROM passwords WHERE name=?", (pw_name,))
         row = self.cursor.fetchone()
 
         if row:
-            # Perform the deletion from the database
             self.cursor.execute("DELETE FROM passwords WHERE name=?", (pw_name,))
             self.conn.commit()
             print(f"Password for {pw_name} has been removed.")
         else:
             print(f"Password for {pw_name} not found.")
 
+    # Clear the console (placeholder function)
     def clear_console(self):
         pass
 
-
+# Run the password manager
 if __name__ == "__main__":
     password_manager = PasswordManager()
     while True:
